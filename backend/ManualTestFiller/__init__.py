ba7736be68d760034d1562262e778d14be1a35dc
Clement FRANCOIS <clement.francois.developpeur@outlook.com>
2025-11-09 09:42:11 +0100

    develop: INSTANTIATE the develop branch:
    + MODIFY the __init__.py from the backend of the application
    + MODIFY the config.py from the backend to set the Config base class, having both DevConfig as well as ProdConfig inherit from it, so that the __init__ can run the desired config depending on where you run the app
    + CREATE a logging_config file, that will setup the log of the application, indeed this app is supposed to be containerized, that means we should have an easy way to fetch logs from it
    + CREATE manage.py that will be used to initializing the database of the application depending on the configuration we are in
    + CREATE run.py that will run the application, this is the file that Dockerfile will run once it finish its initialization

--- backend/ManualTestFiller/__init__.py
+++ backend/ManualTestFiller/__init__.py
@@ -1,17 +1,25 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from .logging_config import setup_logging
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
-    app.config.from_object('ManualTestFiller.config.DevConfig')
+    # Check the environment from the FLASK_ENV
+    env = os.environ.get('FLASK_ENV', 'dev')
+    if env == 'prod':
+        app.config.from_object('ManualTestFiller.config.ProdConfig')
+    else:
+        app.config.from_object('ManualTestFiller.config.DevConfig')
 
-    db.init_app(app)
+    setup_logging(app=app)
+
+    app.logger.info(f"Running in DEBUG={app.config['DEBUG']}")
 
     with app.app_context():
         from . import routes, models
-        db.create_all()
 
     return app
--- backend/ManualTestFiller/config.py
+++ backend/ManualTestFiller/config.py
@@ -1,3 +1,13 @@
-class DevConfig:
-    SQLALCHEMY_DATABASE_URI = 'sqlite://manual_test.db'
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev_secret')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProdConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', 'sqlite:///manual_test.db')
+
+class DevConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///manual_test.db'
